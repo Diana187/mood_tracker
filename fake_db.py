295e733cf4f2c0a310d5eb81a47e63db77804db7
Диана Амирханова <dinaami187@gmail.com>
2023-12-11 18:24:22 +0000

    generate random user_id and mood_id

--- fake_db.py
+++ fake_db.py
@@ -34,7 +34,7 @@ def setup_database():
             password TEXT
             );'''
     )
-    
+
     lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.'
     
     lorem_ipsum_set  = set([i.strip(', .').capitalize() for i in lorem_ipsum.split(' ')])
@@ -66,11 +66,11 @@ def setup_database():
 
     """Creating table 'moods'."""
     cur.execute(
-    '''CREATE TABLE IF NOT EXISTS moods (
-        mood_id INTEGER PRIMARY KEY,
-        mood_name TEXT NOT NULL,
-        mood_rate INTEGER
-        );'''
+        '''CREATE TABLE IF NOT EXISTS moods (
+            mood_id INTEGER PRIMARY KEY,
+            mood_name TEXT NOT NULL,
+            mood_rate INTEGER
+            );'''
     )
 
     mood_rate = {'excellent': 2, 'good': 1, 'normal': 0, 'bad': -1, 'terrible': -2}
@@ -79,6 +79,46 @@ def setup_database():
         cur.execute(
             '''INSERT INTO moods (mood_name, mood_rate) VALUES (?, ?);''', (mood, rate, )
         )
+
+    """Creating table 'records'."""
+    cur.execute(
+        '''CREATE TABLE IF NOT EXISTS records (
+            record_id INTEGER PRIMARY KEY,
+            user_id INTEGER,
+            mood_id INTEGER,
+            mood_description TEXT,
+            activity TEXT,
+            record_date DATETIME,
+            FOREIGN KEY (user_id) REFERENCES users(user_id),
+            FOREIGN KEY (mood_id) REFERENCES moods(mood_id)
+            );'''
+    )
+
+    for record in range(10):
+        user_id = random.randint(1, 43)
+        mood_id = random.randint(1, 5)
+        cur.execute(
+            '''INSERT INTO records (user_id, mood_id) VALUES (?, ?);''', (user_id, mood_id, )
+        )
+
+    # заполнять: рандомный int из диапозона от 1 до количества user (43), mood (5)
+    # границы диапозона тоже должны входить 
+    # заполнить record_date рандомный таймстамп из диапозона (смотри тг)
+
+    """Creating table 'records_to_tags'."""
+    cur.execute(
+        '''CREATE TABLE IF NOT EXISTS records_to_tags (
+            record_tag_id INTEGER PRIMARY KEY,
+            record_id INTEGER,
+            tag_id INTEGER,
+            FOREIGN KEY (record_id) REFERENCES records(record_id),
+            FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+            );'''
+    )
+
+    # NUMBER_OF_RECORDS; 
+
+
 
     conn.commit()
     cur.close()
