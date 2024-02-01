32ee81e33813eeb532d78720345c958c6302c8e2
Диана Амирханова <dinaami187@gmail.com>
2024-02-01 20:02:55 +0000

    containes with rows added & input, update_graph

--- app_2.py
+++ app_2.py
@@ -3,6 +3,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 from datetime import datetime
 from dash import Dash, html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
@@ -11,7 +12,7 @@ from faker import Faker
 
 faker = Faker()
 
-def df_for_chart(num_users=10, count=100, past='-3mo'):
+def df_for_chart(num_users=10, count=100):
 
     users = [f"{faker.first_name()} {faker.last_name()}" for _ in range(num_users)]
     tags = ['foo', 'bar', 'buzz', 'boo', 'puk', 'srenk']
@@ -43,34 +44,56 @@ df = df_for_chart(count=1000)
 
 
 app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# добавить поле для ввода
-# сложить лэйаут в внешний контейнер
-# добавить кнопку (см ноушен, инпут для количества юзеров)
-
-# оба дропдауна сложить в одну строчку: слева и справа, чтобы делили экран на пополам;
-# сложить в dbc.row, объявляю две колонки и в каждой колонце по дропдауну
-# посмотреть доку, чтобы задать ширину
 # добавить простой стиль
-app.layout = html.Div([
+
+default_value = 10
+
+app.layout = dbc.Container([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.tags.unique(), df.tags.unique(), id='dropdown-selection', multi=True),
-    dcc.Dropdown(df.names.unique(), df.names.unique()[0], id='dropdown-selection_name'),
-    dcc.Graph(id='graph-content')
+    dbc.Row([
+        dbc.Col([
+            # html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+            dcc.Dropdown(df.tags.unique(), df.tags.unique(), id='dropdown-selection', multi=True),
+        ], width={'size': 6, 'order': 2}),
+        dbc.Col([
+            dcc.Dropdown(df.names.unique(), df.names.unique()[0], id='dropdown-selection_name'),
+        ],
+        width={'size': 6, 'order': 2}),
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Input(
+                id='input_count',
+                type='number',
+                placeholder='Enter the number of records',
+                value=default_value
+            ),
+            dcc.Graph(id='graph-content'),
+        ], 
+        width=12),
+    ]),
 ])
 
-# добавить инпут
+# сейчас df генерирует новые тестовые данные: добавляю тег - данные обновляются
+# когда меняем теги и имена пусть не генерирует новые данные, а только когда даём новый каунт
+
 @callback(
     Output('graph-content', 'figure'),
     [Input('dropdown-selection', 'value'),
-    Input('dropdown-selection_name', 'value')],
+    Input('dropdown-selection_name', 'value'),
+# добавить инпут
+    Input('input_count', 'value')],
     prevent_initial_call=True
 )
 
 # добавить ещё один аргумент
-def update_graph(selected_tags, selected_name):
-    # dff = df[df.tags.isin(selected_tags) & df.names.isin(selected_names)]
+def update_graph(selected_tags, selected_name, record_count):
+    df = df_for_chart(count=record_count)
     dff = df[df['tags'].isin(selected_tags) & (df.names == selected_name)]
+    # dff = df[df.tags.isin(selected_tags) & df.names.isin(selected_names)]
     # dff = df[(df.tags == selected_tag) & (df.names == selected_name)]
     return px.line(dff, x='times', y='moods')
 
