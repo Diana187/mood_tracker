15b99fbe55e6c9f927e130d32f5b6e67c1cc7b99
Диана Амирханова <dinaami187@gmail.com>
2024-03-24 18:15:09 +0000

    button replaced

--- app_2.py
+++ app_2.py
@@ -1,15 +1,17 @@
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-
-from dash import Dash, html, dcc, callback, Output, Input, State
-import dash_bootstrap_components as dbc
-import plotly.express as px
-import pandas as pd
-from datetime import datetime as dt
 
 import random
+from datetime import datetime as dt
+
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+from dash import Dash, Input, Output, State, callback, dcc, html
+# from dash.exceptions import PreventUpdate
 from faker import Faker
- 
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
 faker = Faker()
 
 def df_for_chart(num_users=10, count=100):
@@ -31,29 +33,6 @@ def df_for_chart(num_users=10, count=100):
 
     return df
 
-    # times = []
-    # for i in range(count):
-    #     time = faker.date_time_between_dates(
-    #         datetime_start=datetime(2016, 1, 1),
-    #         datetime_end=datetime(2016, 7, 21),
-    #         tzinfo=None,
-    #     )
-    #     times.append(time)
-
-    # d = {
-    #     'names': [random.choice(users) for _ in range(count)],
-    #     'times': times,
-    #     'moods': [random.randrange(-2, 3) for _ in range(count)],
-    # }
-    # df = pd.DataFrame(data=d)
-    # df_tags = pd.DataFrame({'tags': tags})
-    # df = df.merge(df_tags, how='cross')
-    # df = df.sample(count).reset_index().drop(columns=['index'])
-    # df['times'] = pd.to_datetime(df['times'])
-    # df.sort_values(by='times', inplace=True)
-
-    # return df
-
 df = df_for_chart(count=1000)
 
 
@@ -63,6 +42,21 @@ app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 default_value = 10
 initial_df = df_for_chart()
 initial_tags = initial_df.tags.unique().tolist()
+
+modal = dbc.Modal(
+    [
+        dbc.ModalHeader('Confirmation'),
+        dbc.ModalBody('Do you want to regenerate the dataframe?'),
+        dbc.ModalFooter(
+            dbc.Button('Yes', id='confirm-button')
+        ),
+    ],
+    id='modal',
+    is_open=False,
+)
+store = dcc.Store(id='modal-state')
+
+
 
 app.layout = dbc.Container([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
@@ -92,12 +86,13 @@ app.layout = dbc.Container([
                 placeholder='Enter the number of records',
                 value=default_value
             ),
+            html.Button('update dataframe', id='popup-button'),
+                modal,
+                dcc.Store(id='df-store'),
             dcc.Graph(id='graph-content'),
         ], 
         width=12),
     ]),
-    # html.Button('update dataframe', id='popup-button'),
-    dcc.Store(id='df-store'),
 ])
 
 # сейчас df генерирует новые тестовые данные: добавляю тег - данные обновляются
@@ -110,27 +105,28 @@ app.layout = dbc.Container([
 # опа https://dash.plotly.com/determining-which-callback-input-changed
 # dash.callback_context, но всё равно не совсем понимаю, как именно нужно применить
 
+@app.callback(
+    Output('modal', 'is_open'),
+    [Input('popup-button', 'n_clicks'),
+     Input('confirm-button', 'n_clicks')],
+    [State('modal', 'is_open')],
+)
+def toggle_modal(popup_clicks, confirm_clicks, is_open):
+    if popup_clicks:
+        return not is_open
+    return is_open
 
 @callback(
     Output('graph-content', 'figure'),
-    # Output('container-button-basic', 'children'), если у функции 2 аутпута, она должна ретёрнить 2 штуки
     [Input('dropdown-selection-tag', 'value'),
-    Input('dropdown-selection-name', 'value')],
-    State('df-store', 'data'),
+    Input('dropdown-selection-name', 'value'),
+    Input('df-store', 'data')],
     prevent_initial_call=True
 )
-
 def update_graph(selected_tags, selected_name, graph_data):
 
-    # в graph_data у меня лежит список кортежей, делаю из них датафрейм
     df = pd.DataFrame(graph_data)
     dff = df[df['tags'].isin(selected_tags) & (df.names == selected_name)]
-
-    # df = df_for_chart(count=record_count)
-    # dff = df[df['tags'].isin(selected_tags) & (df.names == selected_name)]
-    
-    # dff = df[df.tags.isin(selected_tags) & df.names.isin(selected_names)]
-    # dff = df[(df.tags == selected_tag) & (df.names == selected_name)]
     return px.line(dff, x='times', y='moods')
 
 @callback(
@@ -139,29 +135,18 @@ def update_graph(selected_tags, selected_name, graph_data):
     Output('dropdown-selection-tag', 'options'),
     Output('dropdown-selection-tag', 'value'),
     Output('df-store', 'data'),
-    Input('input_count', 'value'),
+    Input('confirm-button', 'n_clicks'),
+    State('input_count', 'value'),
     suppress_callback_exceptions=True
 )
-
-def reset_data(record_count):
+def reset_data(confirm_clicks, record_count):
     df = df_for_chart(count=record_count)
-        
+
     tags = df.tags.unique().tolist()
     names = df.names.unique().tolist()
 
-    return names, names[0], tags, tags, df.to_dict('records')
+    # return names, names[0], tags, tags, df.to_dict('records')
+    return names, names[0], tags, tags, df.to_dict('list')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# зачем мы делаем df.to_dict('records')? и что вообще это такое, чем он отличается от df?
-
-# to_dict('records') – список словарей
-    
-# превращаем DataFrame df в список словарей,
-# каждый словарь из ключа(название столбца)-значения(строки) – строка в DataFrame
-# Plotly/Dash ест только сериализируемые данные, JSON
-# когда посылаем данные из коллбэка в Dash,
-# используем формат, который можно легко сериализовать
-# 
